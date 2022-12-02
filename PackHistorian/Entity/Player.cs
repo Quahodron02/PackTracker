63f2ace74787fc26ebd6c7d022b7606bafd6e322
SGKoishi <i@sgkoi.dev>
2022-12-01 19:46:07 -0800

    Update pack names, multi-account support preparation

---
+++ PackHistorian/Entity/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PackTracker.Entity
+{
+    public class Player
+    {
+        public string BattleTag { get; }
+        public HearthDb.Enums.Region Region { get; }
+
+        public Player(string battleTag, HearthDb.Enums.Region region)
+        {
+            this.BattleTag = battleTag;
+            this.Region = region;
+        }
+    }
+}
