5b7f5fbebee264c24d9b84a84a33a30b7e04116b
SGKoishi <i@sgkoi.dev>
2023-08-05 22:14:18 -0700

    Virtualizing card combobox fix #38

--- PackHistorian/View/ManualPackInsert.cs
+++ PackHistorian/View/ManualPackInsert.cs
@@ -80,7 +80,7 @@ namespace PackTracker.View
 
         private readonly Dictionary<int, List<HDTCard>> _setsCache = new Dictionary<int, List<HDTCard>>();
 
-        private static readonly List<int> _golden = new List<int> { 23, 603, 643, 686 };
+        private static readonly List<int> _golden = new List<int> { 23, 603, 643, 686, 716, 737, 841, 850, 874, 904, 921, 932, 937, 938, 939 };
         private static readonly Dictionary<int, Func<HearthDb.Card, bool>> _filter = new Dictionary<int, Func<HearthDb.Card, bool>>
         {
             [1] = card => card.Set == CardSet.EXPERT1,
